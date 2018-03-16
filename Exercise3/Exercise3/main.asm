dfc88f5329775c6da519a05bed1820b91837c05e
talitapavlova <talitapavlova@gmail.com>
2018-03-16 07:35:06 +0100

    Solved exercise 3

---
+++ Exercise3/Exercise3/main.asm
@@ -0,0 +1,12 @@
+;
+; Exercise3.asm
+;
+; Created: 3/16/2018 7:27:58 AM
+; Author : Talita
+;
+
+
+	ldi r16, 42					; loads 42 (0x2A) into register 16
+	ldi r17, 0x33				; loads 0x33 into register 17
+	ldi r18, 0b1011_0010		; loads 0b1011_0010 (0xB2, 178)
+	nop
